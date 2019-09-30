ae79ad9fd9229325bda065f4896ba96ee5915f5f
斯普林菲尔德 <810461372@qq.com>
2019-09-30 17:33:43 +0800

    系统管理页面效果完成 用户管理页面增删改查完成

--- src/components/footer/foot.vue
+++ src/components/footer/foot.vue
@@ -1,18 +1,22 @@
 <template>
   <!-- 底部 -->
-  <van-tabbar v-model="active" class="vant-bar TabBar">
-    <router-link class="van-tabbar-item" to="/">
-      <van-tabbar-item icon="home-o">首页</van-tabbar-item>
-    </router-link>
-    <router-link class="van-tabbar-item" to="/publicdetails">
-      <van-tabbar-item icon="search">公厕详情</van-tabbar-item>
-    </router-link>
-    <router-link class="van-tabbar-item" to="/ToiletManagement">
-      <van-tabbar-item icon="friends-o">公厕管理</van-tabbar-item>
-    </router-link>
-    <router-link class="van-tabbar-item" to="/Management">
-      <van-tabbar-item icon="search">系统管理</van-tabbar-item>
-    </router-link>
+  <van-tabbar v-model="active" route class="vant-bar TabBar">
+    <van-tabbar-item 
+      replace 
+      to="/" 
+      icon="home-o">首页</van-tabbar-item>
+    <van-tabbar-item 
+      replace 
+      to="/publicdetails" 
+      icon="search">公厕详情</van-tabbar-item>
+    <van-tabbar-item 
+      replace 
+      to="/ToiletManagement" 
+      icon="friends-o">公厕管理</van-tabbar-item>
+    <van-tabbar-item 
+      replace 
+      to="/Management" 
+      icon="setting-o">系统管理</van-tabbar-item>
   </van-tabbar>
 </template>
 
