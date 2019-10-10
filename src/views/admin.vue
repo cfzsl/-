57ffa9ee6b261520edc67d81095415deb10ac17a
zsl <2438853928@qq.com>
2019-10-10 18:18:48 +0800

    测试

---
+++ src/views/admin.vue
@@ -0,0 +1,23 @@
+<template>
+  <!-- 登录之前的页面 -->
+  <div class='lodin'>
+      <div class='lodinCenter'>
+          东区智慧公厕监管平台
+      </div>
+      <div class='lodinBot'>
+          <p>技术支持：东营市旭瑞智能科技股份有限公司</p>
+          <p>联系电话：0546-7</p>
+      </div>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+ 
+};
+</script>
+
+<style scoped>
+</style>
